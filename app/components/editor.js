825c13d37426384f07e4820bdb953cab796d0e3d
Ollie Khakwani <okhakwani@gmail.com>
2017-09-02 13:03:27 -0400

    added content property to state in editor.js, made Editor into controlled component

--- app/components/editor.js
+++ app/components/editor.js
@@ -1,10 +1,20 @@
 import React, { Component } from 'react';
 
 class Editor extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { content: '' };
+  }
+
   render() {
     return (
       <div>
-        <textarea />
+        <h1>Editor</h1>
+        <textarea
+          value={this.state.content}
+          onChange={event => this.setState({content: event.target.value})} />
+        <p>Editor input: {this.state.content}</p>
       </div>
     );
   }
