b84c7a9b212f506e7109c33fa82415cd8e61d01b
Ollie Khakwani <okhakwani@gmail.com>
2017-09-04 11:49:41 -0400

    added skeleton for functional previewer component

---
+++ app/components/previewer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Previewer = ({content}) => {
+  return (
+    <div>
+      <h1>Previewer</h1>
+      <div>{content}</div>
+    </div>
+  )
+};
+
+export default Previewer;
--- app/index.js
+++ app/index.js
@@ -1,14 +1,26 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import Editor from './components/editor';
+import Previewer from './components/previewer';
 
-const App = () => {
-  return (
-    <div>
-      Testing
-      <Editor />
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      content: 'testing'
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        Testing
+        <Editor />
+        <Previewer content={this.state.content} />
+      </div>
+    );
+  }
 }
 
 ReactDOM.render(<App />, app);
